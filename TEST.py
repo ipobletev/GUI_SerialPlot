e42e5b88446cb2dbbf119101f9ab92f5f0f336f0
ismaelPoblete <ismael.pbt@gmail.com>
2021-05-11 17:43:40 -0400

    fix(graph Plot)

--- TEST.py
+++ TEST.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from HMI_SerialPlot import *
 from PySide2.QtCore import QTimer
 from SME_SerialCom import SME_Serial_Communication
@@ -7,7 +8,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 #https://www.mfitzp.com/tutorials/plotting-pyqtgraph/
-
+from random import randint
 class MyApp(QMainWindow):
     def __init__(self,*args,**kwargs):
         super().__init__()
@@ -29,34 +30,28 @@ class MyApp(QMainWindow):
 
         # Set up GUI configuration
 
-        self.graphWidget = pg.PlotWidget()
-        #self.setCentralWidget(self.graphWidget)
-
         self.x = list(range(100))  # 100 time points
-        self.y = [0 for _ in range(100)] # 100 data points
-
+        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
         #Add Background colour to white
-        self.graphWidget.setBackground('w')
+        self.ui.graphicsView.setBackground('w')
         # Add Title
-        self.graphWidget.setTitle("Your Title Here", color="b", size="30pt")
+        self.ui.graphicsView.setTitle("Your Title Here", color="b", size="15pt")
         # Add Axis Labels
         styles = {"color": "#f00", "font-size": "20px"}
-        self.graphWidget.setLabel("left", "X1 (°C)", **styles)
-        self.graphWidget.setLabel("bottom", "X2 (H)", **styles)
+        self.ui.graphicsView.setLabel("left", "Voltage [V]", **styles)
+        #self.ui.graphicsView.setLabel("bottom", "TIME", **styles)
         #Add legend
-        self.graphWidget.addLegend()
+        self.ui.graphicsView.addLegend()
         #Add grid
-        self.graphWidget.showGrid(x=True, y=True)
+        self.ui.graphicsView.showGrid(x=True, y=True)
         #Set Range
-        self.graphWidget.setXRange(0, 100, padding=0)
-        self.graphWidget.setYRange(0, 5, padding=0)
-
-        pen = pg.mkPen(color=(255, 0, 0), width=5, style=QtCore.Qt.SolidLine)
-        self.graphWidget.plot(self.x, self.y, name="Sensor 1",  pen=pen)
-
-        #self.graphWidget.clear()
-
-
+        self.ui.graphicsView.setXRange(0, 100, padding=0)
+        self.ui.graphicsView.setYRange(0, 5, padding=0)
+        #Set legend plot
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line =  self.ui.graphicsView.plot(self.x, self.y, pen=pen)
+        self.ui.graphicsView.clear()
+        self.ui.graphicsView.resize(500,200)
 
     def data_serial(self,data):
         self.data_1 = float(data)
@@ -69,7 +64,7 @@ class MyApp(QMainWindow):
         self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
 
         self.y = self.y[1:]  # Remove the first
-        self.y.append(self.data_1)  # Add a new random value.
+        self.y.append( self.data_1)  # Add a new random value.
 
         self.data_line.setData(self.x, self.y)  # Update the data.
 
