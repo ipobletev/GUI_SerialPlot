073a72a141c575fbaadb7d2d8ff4b490f3824c66
ismaelpob <ismael.pbt@gmail.com>
2021-05-16 11:29:22 -0400

    Comments and project description added

--- Main_SerialPlot.py
+++ Main_SerialPlot.py
@@ -1,38 +1,41 @@
 ################################################################################
-## Proyect: 
+## Proyect: SerialPlot
 ## Autor: Ismael Poblete
-## Date: 13-05-2021
-## Brief: Program
-## 
+## Company: -
+## Date: 16-05-2021
+## Brief: Plot graph via serial com. Tested with arduino device.
+## Lib: PyQt5, pygraph, SME (SerialCom, DataLooger).
 ##
 ################################################################################
 
+################################################################################
 ###IMPORTS
 import sys
 import os
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsDropShadowEffect, QMessageBox
-from PyQt5.QtCore import Qt, QTimer
-from pyqtgraph import PlotWidget, plot
-import pyqtgraph as pg
-
-from PyQt5 import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-
 from datetime import datetime
 
-###SME
+#SME
 from SME_SerialCom import SME_Serial_Communication
 from SME_DataLogger import *
 
-## ==> SPLASH SCREEN
+#PyQt5
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsDropShadowEffect, QMessageBox
+from PyQt5.QtCore import Qt, QTimer
+#pygtgraph
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+
+##Qt5 windows
+# Splash screen
 from UI_SplashScreen import *
-## ==> MAIN WINDOW
+# Main windows
 from UI_MainWindow import *
 
+################################################################################
 
-
+#Main Windows
 class MyApp(QMainWindow):
     def __init__(self,*args,**kwargs):
         super().__init__()
@@ -55,25 +58,25 @@ class MyApp(QMainWindow):
         self.serial.data_receive.connect(self.update_data)
         self.ui.ComboBox_Baud.addItems(self.serial.baudrates)
         self.ui.ComboBox_Baud.setCurrentText("9600")
-        #Data of serial COM
         self.data_1=0.0
+
         #Set indicator level off not connected
         self.ui.Indicador.setStyleSheet("border-radius:130px;\n"
-"background-color:rgb(225, 225, 225);\n"
-"")     
+        "background-color:rgb(225, 225, 225);\n"
+        "")     
+
         #Init timer for serial check
         self.timer=QTimer()
         self.timer.timeout.connect(self.check_serial_status)
         self.timer.start(500)
 
-
-        #Configure Range Type of X axis
+        #Configure combobox Range of X axis
         self.ui.ComboBox_rangetype.addItems(['Static','AutoAdjustX'])
         self.ui.ComboBox_rangetype.setCurrentText("Static")
         self.range_type = "Static"
         self.spinBox_range_xvalue = self.range_x_data
         
-        #Axis configuration
+        #Axis spinBix and labels configuration
         self.ui.spinBox_limit_py.setMaximum(999999999)
         self.ui.spinBox_limit_py.setMinimum(-999999999)
         self.ui.spinBox_limit_ny.setMaximum(999999999)
@@ -90,20 +93,11 @@ class MyApp(QMainWindow):
         self.ui.TL_label_y.setText("")
         self.ui.TL_label_x.setText("")
         self.ui.TL_label_time.setText(str(self.timeacquisition))
-
-        #Configure the conects of Buttons and Methods
-        self.ui.BT_Refresh.clicked.connect(self.clickbutton_refresh)
-        self.ui.BT_Connect.clicked.connect(self.clickbutton_connect)
-        self.ui.BT_Disconnect.clicked.connect(self.clickbutton_disconnect)
-        self.ui.BT_Clear.clicked.connect(self.clickbutton_cleardata)
-        self.ui.Check_datapoint.stateChanged.connect(self.check_dataplot)
-        self.ui.Check_Record.stateChanged.connect(self.data_acquisition)
-
-        # Set Up Plot Graph
+        
+        ### Set Up Plot Graph
         self.x=[]
         self.y=[]
         self.cont_x=0;
-
         #Add Background colour to white
         self.ui.graphicsView.setBackground('w')
         # Add Title
@@ -117,27 +111,29 @@ class MyApp(QMainWindow):
         #Add grid
         self.ui.graphicsView.showGrid(x=True, y=True)
         #Set Range
-        
         self.ui.graphicsView.setXRange(0, self.range_x_data, padding=0)
         self.ui.graphicsView.setYRange(0, self.axis_p_y, padding=0)
         self.ui.graphicsView.enableAutoRange(axis=None, enable=True, x=True, y=False)
         #Set legend plot
         self.pen = pg.mkPen(color=(255, 0, 0), width=4, style=QtCore.Qt.SolidLine)
         self.ui.graphicsView.setMouseEnabled(x=False, y=False)
-        
         self.data_line =  self.ui.graphicsView.plot(self.x, self.y, pen=self.pen)
-        
-        #self.data_line =  self.ui.graphicsView.plot(self.x, self.y, pen=pen, symbol='o', symbolPen ='r',symbolSize=5, symbolBrush=0.2, name ='blue')
-        #self.data_line.setSymbolPen(QColor(220, 30, 30))
-
         # Add crosshair lines.
         self.crosshair_v = pg.InfiniteLine(angle=90, movable=False)
         self.crosshair_h = pg.InfiniteLine(angle=0, movable=False)
         self.ui.graphicsView.addItem(self.crosshair_v, ignoreBounds=True)
         self.ui.graphicsView.addItem(self.crosshair_h, ignoreBounds=True)
-        
         self.proxy = pg.SignalProxy(self.ui.graphicsView.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
-    
+        
+        #Configure the conects of Buttons and Methods
+        self.ui.BT_Refresh.clicked.connect(self.clickbutton_refresh)
+        self.ui.BT_Connect.clicked.connect(self.clickbutton_connect)
+        self.ui.BT_Disconnect.clicked.connect(self.clickbutton_disconnect)
+        self.ui.BT_Clear.clicked.connect(self.clickbutton_cleardata)
+        self.ui.Check_datapoint.stateChanged.connect(self.check_dataplot)
+        self.ui.Check_Record.stateChanged.connect(self.data_acquisition)
+
+    #Method: Check serial status via TIMER and execute a pop up windows in case a error
     def check_serial_status(self):
         if(self.serial.alive.isSet()):
             self.ui.Indicador.setStyleSheet("border-radius:130px;\n"
@@ -158,50 +154,46 @@ class MyApp(QMainWindow):
             msg.setStandardButtons(QMessageBox.Ok)
             x = msg.exec_()
 
+    #Method: If SME_SerialCom acquire data then this function will execute and give data value
     def update_data(self,data):
 
+        #Set Y axis
         limit_axis_py = self.ui.spinBox_limit_py.value()
         limit_axis_ny = self.ui.spinBox_limit_ny.value()
-
         self.range_type = self.ui.ComboBox_rangetype.currentText()
         self.ui.graphicsView.setYRange(limit_axis_ny, limit_axis_py, padding=0)
 
-        #Save data of serial COM
+        #Store data serial COM
         self.data_1 = (float(data) * float(self.ui.TL_factor_multiplier.text())) + float(self.ui.TL_factor_sum.text())
-        
-        #Process data
+        self.timeacquisition = float(self.ui.TL_factor_time.text()) * float(self.cont_x)
+
+        #Process data into the visual graph
         if((len(self.y)>int(self.range_x_data)) and (self.range_type == "Static")):
 
             self.x = self.x[1:]  # Remove the first X element.
             self.y = self.y[1:]  # Remove the first Y element.
 
         self.x.append(self.cont_x)  # Add a new value 1 higher than the last.
-        self.y.append(self.data_1)  # Add a new random value.
+        self.y.append(self.data_1)  # Add a new value.
         
-        self.timeacquisition = float(self.ui.TL_factor_time.text()) * float(self.cont_x)
-
-        #Update Text Label data
+        #Update Text Label with acquisition data
         self.ui.TL_label_x.setText(str(format(self.cont_x, '.1f')))
         self.ui.TL_label_y.setText(str(format(self.data_1, '.3f')))
         self.ui.TL_label_time.setText(str(format(self.timeacquisition, '.1f')))
-     
         #Update Plot Graph
         self.data_line.setData(self.x, self.y)
-       
-        #Save data acquisition
+        #Store in CSV data acquisition, if is the case.
         if(self.flag_data_acquisition):
             self.data_logger.SME_DataLogger_SaveData([self.cont_data, format(self.data_1, '.3f')])
             self.cont_data+=1
-     
         self.cont_x+=1
     
+    #Detect every mouse change in the plot graph and calculate the data plot values
     def mouseMoved(self, e):
         pos = e[0]
         if self.ui.graphicsView.sceneBoundingRect().contains(pos):
             mousePoint = self.ui.graphicsView.getPlotItem().vb.mapSceneToView(pos)
             self.mouse_x = format(mousePoint.x(), '.0f')
-
-            
 
             if (int(self.mouse_x) < int(self.cont_x)):
                 self.crosshair_v.setPos(mousePoint.x())
@@ -216,7 +208,8 @@ class MyApp(QMainWindow):
                     self.ui.TL_cursor_y.setText(str(self.y[self.location_y]))
                 except:
                     pass
-
+    
+    #Check dataplot and change the graph with points
     def check_dataplot(self):
         self.data_line.clear()
         if(self.ui.Check_datapoint.checkState()):
@@ -225,6 +218,7 @@ class MyApp(QMainWindow):
         else :
             self.data_line =  self.ui.graphicsView.plot(self.x, self.y, pen=self.pen)
 
+    #ClickButton: Connect to SerialCOM
     def clickbutton_connect(self):
         self.clickbutton_cleardata()
         index = self.ui.ComboBox_PortSerial.currentIndex()
@@ -234,25 +228,25 @@ class MyApp(QMainWindow):
         self.serial.serial_com.baudrate = baud
         self.serial.serial_connection()
 
+    #ClickButton: Disconnect to SerialCOM
     def clickbutton_disconnect(self):
         self.serial.serial_disconnect()
 
+    #ClickButton: Refresh to find Serial COM
     def clickbutton_refresh(self):
         self.serial.ports_availables()
         self.ui.ComboBox_PortSerial.clear()
         self.ui.ComboBox_PortSerial.addItems(self.serial.text_serial_ports)
 
+    #ClickButton: reset data and graph
     def clickbutton_cleardata(self):
         self.ui.TL_label_x.setText("")
         self.ui.TL_label_y.setText("")
-
         #Get the new range value and clear plot x and y data
         self.range_x_data = self.ui.spinBox_range_xvalue.value()
         self.x.clear()
         self.y.clear()
         self.x=[]
-        self.y=[]
-
         #Set limits 1________2
         limit_range_x_1 = self.cont_x
         limit_range_x_2 = limit_range_x_1 + self.range_x_data
@@ -261,6 +255,7 @@ class MyApp(QMainWindow):
         self.cont_x=0
         self.timeacquisition=0.0
 
+    #Data acquisition: If is activate, create a folder and csv file .
     def data_acquisition(self):
         
         if(self.ui.Check_Record.checkState()):
@@ -278,6 +273,7 @@ class MyApp(QMainWindow):
         else:
             self.flag_data_acquisition = False         
 
+#Splash Screen Windows
 class SplashScreen(QMainWindow):
     def __init__(self):
         super().__init__()
